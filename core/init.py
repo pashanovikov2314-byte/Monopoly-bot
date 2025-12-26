68fbf9a206a44e508d86801d4a60e59c8adbf087
pashanovikov2314 <pashanovikov2314@gmail.com>
2025-12-26 23:02:05 +0300

    CLEAN: Удалил поврежденные файлы, оставил только рабочую минимальную версию

--- core/init.py
+++
@@ -1,6 +0,0 @@
-"""
-Core package for Monopoly Premium Bot
-"""
-
-__version__ = "2.5.0"
-__author__ = "Dark Prince (@Whylovely05)"
--- utils/scheduler.py
+++
@@ -1,6 +0,0 @@
-﻿class GameScheduler:
-    async def start(self, bot, db):
-        print('Планировщик игр запущен')
-        
-    async def stop(self):
-        print('Планировщик игр остановлен')
