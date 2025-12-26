8692a67f14450ca98472b6ef99dc501ee0c9c44e
pashanovikov2314 <pashanovikov2314@gmail.com>
2025-12-26 21:50:14 +0300

    ADD: Тест зависимостей

---
+++ test_deps.py
@@ -0,0 +1,14 @@
+﻿import sys
+print("Python version:", sys.version)
+
+try:
+    import aiogram
+    print("✅ aiogram импортирован, версия:", aiogram.__version__)
+except ImportError as e:
+    print("❌ aiogram НЕ импортирован:", e)
+    
+try:
+    import aiohttp
+    print("✅ aiohttp импортирован")
+except ImportError:
+    print("❌ aiohttp НЕ импортирован")
